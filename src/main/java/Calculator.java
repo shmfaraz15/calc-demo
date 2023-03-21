68a275f60b01957b846bca6122e2fa487b9a3967
faraz <shm.faraz@15gmail.com>
2023-03-21 16:36:38 +0530

    Changes working

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,105 +3,110 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Calculator extends JFrame implements ActionListener {
-
+    private static final long serialVersionUID = 1L;
     private JTextField textField;
     private JButton[] buttons;
     private String[] buttonLabels = {
-            "1", "2", "3", "+",
-            "4", "5", "6", "-",
-            "7", "8", "9", "*",
-            ".", "0", "=", "/",
-            "x!", "x^y", "sqrt", "ln"
+            "7", "8", "9", "x^2",  "4", "5", "6","sqrt", "1", "2", "3","x!","0", ".", "=", "ln",  "-", "*",  "+", "C"
     };
+    private double firstOperand;
+    private String operator;
+    private boolean secondOperand;
 
     public Calculator() {
-        setTitle("Calculator");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Scientific Calculator");
         setSize(300, 300);
-        setLayout(new BorderLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
 
-        // Add text field for input and display
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
         textField = new JTextField();
-        add(textField, BorderLayout.NORTH);
+        textField.setEditable(false);
+        panel.add(textField, BorderLayout.NORTH);
 
-        // Add number pad buttons
-        JPanel buttonPanel = new JPanel(new GridLayout(5, 4));
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(5, 4));
         buttons = new JButton[buttonLabels.length];
-
         for (int i = 0; i < buttonLabels.length; i++) {
             buttons[i] = new JButton(buttonLabels[i]);
             buttons[i].addActionListener(this);
             buttonPanel.add(buttons[i]);
         }
+        panel.add(buttonPanel, BorderLayout.CENTER);
 
-        add(buttonPanel, BorderLayout.CENTER);
-
+        add(panel);
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
-        String label = e.getActionCommand();
-
-        // Handle number pad input
-        if (label.matches("\\d") || label.equals(".")) {
-            textField.setText(textField.getText() + label);
-        } else {
-            // Parse input number
-            double num = Double.parseDouble(textField.getText());
-
-            // Perform selected operation
-            switch (label) {
-                case "+":
-                    textField.setText("");
-                    textField.setText(String.valueOf(num + Double.parseDouble(textField.getText())));
-                    break;
-                case "-":
-                    textField.setText("");
-                    textField.setText(String.valueOf(num - Double.parseDouble(textField.getText())));
-                    break;
-                case "*":
-                    textField.setText("");
-                    textField.setText(String.valueOf(num * Double.parseDouble(textField.getText())));
-                    break;
-                case "/":
-                    textField.setText("");
-                    textField.setText(String.valueOf(num / Double.parseDouble(textField.getText())));
-                    break;
-                case "x!":
-                    textField.setText("");
-                    textField.setText(String.valueOf(factorial(num)));
-                    break;
-                case "x^y":
-                    textField.setText("");
-                    textField.setText(String.valueOf(Math.pow(num, Double.parseDouble(textField.getText()))));
-                    break;
-                case "sqrt":
-                    textField.setText("");
-                    textField.setText(String.valueOf(Math.sqrt(num)));
-                    break;
-                case "ln":
-                    textField.setText("");
-                    textField.setText(String.valueOf(Math.log(num)));
-                    break;
-                case "=":
-                    // Do nothing
-                    break;
-                default:
-                    // Do nothing
-                    break;
-            }
-        }
-    }
-
-    public static double factorial(double n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
+        String buttonText = ((JButton)e.getSource()).getText();
+        switch (buttonText) {
+            case "C":
+                textField.setText("");
+                firstOperand = 0;
+                secondOperand = false;
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                operator = buttonText;
+                firstOperand = Double.parseDouble(textField.getText());
+                textField.setText("");
+                secondOperand = true;
+                break;
+            case "=":
+                if (secondOperand) {
+                    double secondOperandValue = Double.parseDouble(textField.getText());
+                    double result = 0;
+                    switch (operator) {
+                        case "+":
+                            result = firstOperand + secondOperandValue;
+                            break;
+                        case "-":
+                            result = firstOperand - secondOperandValue;
+                            break;
+                        case "*":
+                            result = firstOperand * secondOperandValue;
+                            break;
+                        case "/":
+                            result = firstOperand / secondOperandValue;
+                            break;
+                    }
+                    textField.setText(Double.toString(result));
+                    secondOperand = false;
+                }
+                break;
+            case "x^2":
+                double operandValue = Double.parseDouble(textField.getText());
+                double squaredValue = operandValue * operandValue;
+                textField.setText(Double.toString(squaredValue));
+                break;
+            case "sqrt":
+                double sqrtValue = Math.sqrt(Double.parseDouble(textField.getText()));
+                textField.setText(Double.toString(sqrtValue));
+                break;
+            case "ln":
+                double lnValue = Math.log(Double.parseDouble(textField.getText()));
+                textField.setText(Double.toString(lnValue));
+                break;
+            case "x!":
+                int factorialValue = Integer.parseInt(textField.getText());
+                int result = 1;
+                for (int i = factorialValue; i > 0; i--) {
+                    result *= i;
+                }
+                textField.setText(Integer.toString(result));
+                break;
+            default:
+                textField.setText(textField.getText() + buttonText);
+                break;
         }
     }
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        new Calculator();
     }
 }
